e1c9919dd8421cc1c3b4056bce483a33ab22241c
ikemasa1617 <ikemasa1106@gmail.com>
2019-05-13 17:27:00 +0900

    ルートの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'messages#index'
-  resources :users, only: [:edit, :update]
+  resources :users, only: [:index, :edit, :update]
+  resources :groups, onlu: [:new, :create, :edit, :update] do
+    resources :messages, only: [:index, :create]
+  end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
