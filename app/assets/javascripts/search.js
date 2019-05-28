9a29cbf51844328e820bb581bad0b64b56e57b12
ikemasa1617 <ikemasa1106@gmail.com>
2019-05-28 11:22:02 +0900

    Update search.js

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -10,6 +10,7 @@ function appendUser(user){
                 </p>
                 <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">
                 追加
+                </div>
               </div>`;
     search_list.append(html);
 }
